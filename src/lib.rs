65e8b6dd24bf83fbb90e4aea2aca1160ebf3254e
Jose Luis Ricon <jose@ricon.xyz>
2019-07-20 18:58:23 +0100

    Add functionality

---
+++ src/lib.rs
@@ -0,0 +1,109 @@
+#[macro_use]
+extern crate clap;
+
+use colored::{ColoredString, Colorize};
+use reqwest::r#async::Response;
+use reqwest::{StatusCode, Url};
+
+use select::document::Document;
+use select::predicate::Name;
+use std::collections::HashSet;
+use std::fmt::Display;
+
+use clap::{App, Arg};
+pub fn print_error<T: Display>(x: T) {
+    let formatted_str = format!("{}", x).bold_red();
+    println!("{}", formatted_str);
+}
+pub fn print_response(x: Response) {
+    match x.status() {
+        StatusCode::OK => {
+            let formatted_str =
+                format!("{} is valid ({})", x.url().as_str(), x.status().as_str()).bold_green();
+            println!("{}", formatted_str);
+        }
+        _ => {
+            let formatted_str =
+                format!("{} failed ({})", x.url().as_str(), x.status().as_str()).bold_red();
+            println!("{}", formatted_str);
+        }
+    }
+}
+pub trait ColorsExt {
+    fn bold_red(&self) -> ColoredString;
+    fn bold_green(&self) -> ColoredString;
+}
+impl ColorsExt for str {
+    fn bold_red(self: &str) -> ColoredString {
+        self.bold().red()
+    }
+    fn bold_green(self: &str) -> ColoredString {
+        self.bold().green()
+    }
+}
+pub fn make_app<'a, 'b>() -> App<'a, 'b> {
+    App::new("Rusty Links")
+        .version(crate_version!())
+        .author("Jose Luis Ricon <jose@ricon.xyz")
+        .about("Finds dead links in websites")
+        .arg(
+            Arg::with_name("INPUT")
+                .help("URL to check links for (e.g. http://www.google.com)")
+                .index(1),
+        )
+}
+#[derive(Debug)]
+pub enum RustyLinksError {
+    MalformedUrl,
+    RequestError,
+}
+fn add_http(url_string: &str) -> String {
+    if !url_string.starts_with("http://") {
+        ["http://", url_string].concat()
+    } else {
+        url_string.to_owned()
+    }
+}
+pub fn get_links_for_website(url_string: String) -> Result<HashSet<String>, RustyLinksError> {
+    let links = Url::parse(&add_http(&url_string)).map(|url| {
+        reqwest::get(url)
+            .map(|doc| match doc.status() {
+                StatusCode::OK => Document::from_read(doc)
+                    .unwrap()
+                    .find(Name("a"))
+                    .filter_map(|n| n.attr("href"))
+                    .map(|x| {
+                        if x.starts_with("/") {
+                            Option::Some([&url_string, &x[1..]].concat())
+                        } else if x.starts_with("http") {
+                            Option::Some(x.to_owned())
+                        } else {
+                            Option::None
+                        }
+                    })
+                    .filter(|elem| elem.is_some())
+                    .map(|elem| match elem {
+                        Some(e) => e,
+                        _ => panic!("This can't happen"),
+                    })
+                    .collect(),
+                _ => {
+                    let err = format!("Could not reach website {}: {}", url_string, doc.status());
+                    print_error(err);
+                    HashSet::new()
+                }
+            })
+            .map_err(|e| println!("{:?}", e))
+    });
+
+    match links {
+        Ok(e) => match e {
+            Ok(e) => Ok(e),
+            Err(_) => Err(RustyLinksError::RequestError),
+        },
+        Err(e) => {
+            println!("{:?}", e);
+            Err(RustyLinksError::MalformedUrl)
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,49 +1,57 @@
-use colored::*;
-use futures::{stream, future, Stream};
+use futures::{future, stream, Stream};
 use reqwest::r#async::Client;
+use rlinks::{print_error, print_response, get_links_for_website, make_app};
+use std::collections::HashSet;
 use std::sync::mpsc;
 use tokio;
-const PARALLEL_REQUESTS: usize = 100;
+const PARALLEL_REQUESTS: usize = 1000;
 
-fn fetch(req: Vec<&'static str>) {
+fn fetch(req: HashSet<String>) {
     let client = Client::new();
-
-    let bodies = stream::iter_ok(req)
-        .map(move |url| client.get(url).send())
-        .buffer_unordered(PARALLEL_REQUESTS);
-
     let (tx, rx) = mpsc::channel();
-
-    let work = bodies
-        .inspect_err(|err| {
-            let m = format!(
-                "Error on {:?}",
-                match err.url() {
-                    Some(u) => u,
-                    _ => panic!("This shouldn't happen"),
-                }
-            )
-            .red()
-            .bold();
-            println!("{}", m);
-        })
-        .then(|r| future::ok(stream::iter_ok::<_, ()>(r)))
-        .flatten()
-        .for_each(move |n| {
+    let req_len = req.len();
+    println!("Checking for dead links...");
+    let work = stream::iter_ok(req)
+        .map(move |url| client.get(&url).send())
+        .buffer_unordered(PARALLEL_REQUESTS)
+        .then(move |response| {
             let this_tx = tx.clone();
-            this_tx.send(1).unwrap();
-            let m = format!("Success on {:?}", n.url()).green().bold();
-            println!("{}", m);
+            match response {
+                Ok(x) => {
+                    print_response(x);
 
-            Ok(())
-        });
+                    this_tx.send(1).unwrap();
+                }
+
+                Err(x) => {
+                    print_error(x);
+                }
+            }
+
+            future::ok(())
+        })
+        .for_each(|_| Ok(()));
 
     tokio::run(work);
     let valid_links: u32 = rx.iter().sum();
-    println!("Got {} valid links", valid_links);
+    println!("Got {}/{} valid links", valid_links, req_len);
 }
+
 fn main() {
-    let mut req = vec!["http://ricon.dev"; 3];
-    req.push("http://a323232.com");
-    fetch(req);
+    let app = make_app().get_matches();
+
+    let url = app.value_of("INPUT");
+
+    match url {
+        Some(e) => {
+            get_links_for_website(e.to_owned())
+                .map(|f| fetch(f))
+                .map_err(|err| println!("{:?}", err));
+        }
+        // If there is no input argument
+        _ => {
+            make_app().print_help().unwrap();
+            println!();
+        }
+    }
 }
