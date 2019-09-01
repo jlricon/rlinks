7f6db7e216f383bcfadfb0a9c0f2a85e5324fc6a
Jose Luis Ricon <jose@ricon.xyz>
2019-09-01 16:47:22 +0100

    Fix example

--- examples/check_req.rs
+++ examples/check_req.rs
@@ -1,19 +1,18 @@
 use futures::{stream, StreamExt};
-use isahc::{Error, HttpClient};
+use isahc::{prelude::Response, Body, Error, HttpClient};
 
 fn get_client() -> HttpClient {
     HttpClient::builder().cookies().build().unwrap()
 }
-async fn make_req(client: &HttpClient, url: &str) -> Result<(), Error> {
-    client.get_async(url).await;
-    Ok(())
+async fn make_req(client: &HttpClient, url: &str) -> Result<Response<Body>, Error> {
+    client.get_async(url).await
 }
 
 #[tokio::main]
 async fn main() {
     let client = get_client();
     let vec_reqs = vec!["https://google.com", "https://github.com"];
-    let res: Vec<Result<(), Error>> = stream::iter(vec_reqs)
+    let _: Vec<Result<Response<Body>, Error>> = stream::iter(vec_reqs)
         .map(|r| make_req(&client, r))
         .buffer_unordered(1)
         .collect()
