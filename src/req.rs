946d1e7b40fa425e14502766b0a99dcc2e10462f
Jeremiah Russell <jrussell@jerus.ie>
2021-05-11 16:30:14 +0100

    Replace GET and HEAD with RFC 430 compliant UpperCamelCase

--- src/req.rs
+++ src/req.rs
@@ -12,7 +12,7 @@ use isahc::{
     config::{Configurable, RedirectPolicy, VersionNegotiation},
     error::ErrorKind,
     prelude::*,
-    AsyncBody, HttpClient, Request, Response,
+    AsyncBody, Body, HttpClient, Request, Response,
 };
 
 use regex::Regex;
